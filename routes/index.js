16712063d37bab37d0aa4f97e5e407b124854aba
fanyinghao <fanyinghao@gmail.com>
2022-01-21 18:43:49 +0800

    add jiang

--- routes/index.js
+++ routes/index.js
@@ -62,7 +62,9 @@ router.post('/wx/call', async function (req, res, next) {
       const UNAME = {
         0: '萌姜',
         1: '小姜',
-        2: '大姜'
+        2: '大姜',
+        3: '辣姜',
+        4: '够姜'
       }
       var rand = Math.floor(Math.random() * Object.keys(UNAME).length);
       var randName = UNAME[Object.keys(UNAME)[rand]];
